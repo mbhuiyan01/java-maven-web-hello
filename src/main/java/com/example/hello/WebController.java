0d1ad9d6e79c0fed15bbcc0ad311c98e4ea2e16a
mbhuiyan01 <48452702+mbhuiyan01@users.noreply.github.com>
2020-06-19 16:23:54 -0400

    Add files via upload

---
+++ src/main/java/com/example/hello/WebController.java
@@ -0,0 +1,21 @@
+package com.example.hello;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class WebController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	String sayHello () {
+		
+		System.out.println("Hello, World!");
+		
+		return "hello";
+	}
+}
